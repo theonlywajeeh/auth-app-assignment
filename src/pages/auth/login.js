2ef873f914e1f2712571836fdd23b52b376f7825
theonlywajeeh <theycallmewajeeh@gmail.com>
2023-08-06 13:01:46 +0500

    updated login.js

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,28 +1,68 @@
 
 
+// import Form from "@/components/auth/form";
+// import { signIn } from "next-auth/react";
+// import { useRouter } from "next/router";
+// import Header from "../../components/header";
+
+// export default function SignIn() {
+//   const router = useRouter();
+
+//   const onSubmit = async (email, password) => {
+//     try {
+//       const data = await signIn("credentials", { redirect: false, email, password });
+
+      
+//       data && router.push("/profile");
+//     } catch (err) {
+//       console.error("Login failed:", err);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <Header />
+//       <Form signin={true} onFormSubmit={onSubmit} />
+//     </div>
+//   );
+// }
+
+
 import Form from "@/components/auth/form";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 import Header from "../../components/header";
+import { useSession, signOut } from "next-auth/react";
 
 export default function SignIn() {
-  const router = useRouter();
+  const { data: session } = useSession();
 
   const onSubmit = async (email, password) => {
     try {
       const data = await signIn("credentials", { redirect: false, email, password });
 
-      
       data && router.push("/profile");
     } catch (err) {
       console.error("Login failed:", err);
     }
   };
 
-  return (
-    <div>
-      <Header />
-      <Form signin={true} onFormSubmit={onSubmit} />
-    </div>
-  );
+  if (session) {
+   
+    return (
+      <div>
+        <Header />
+       <h1> Signed in as {session.user.email} <br /></h1>
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
+    );
+  } else {
+    
+    return (
+      <div>
+        <Header />
+        <Form signin={true} onFormSubmit={onSubmit} />
+      </div>
+    );
+  }
 }
