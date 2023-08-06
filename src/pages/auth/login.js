91c85e868e7cce68b1de42548593456c973b3d21
theonlywajeeh <theycallmewajeeh@gmail.com>
2023-08-06 13:16:37 +0500

    updated login.js

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,32 +1,5 @@
 
 
-// import Form from "@/components/auth/form";
-// import { signIn } from "next-auth/react";
-// import { useRouter } from "next/router";
-// import Header from "../../components/header";
-
-// export default function SignIn() {
-//   const router = useRouter();
-
-//   const onSubmit = async (email, password) => {
-//     try {
-//       const data = await signIn("credentials", { redirect: false, email, password });
-
-      
-//       data && router.push("/profile");
-//     } catch (err) {
-//       console.error("Login failed:", err);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <Header />
-//       <Form signin={true} onFormSubmit={onSubmit} />
-//     </div>
-//   );
-// }
-
 
 import Form from "@/components/auth/form";
 import { signIn } from "next-auth/react";
